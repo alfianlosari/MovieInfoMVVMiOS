0b2c3fa97852c97b4b79bbf3d4b210eba5ea2d29
Alfian Losari <alfianlosari@gmail.com>
2019-03-10 10:52:24 +0530

    Initial Commit

---
+++ MovieInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieInfo
+//
+//  Created by Alfian Losari on 10/03/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
